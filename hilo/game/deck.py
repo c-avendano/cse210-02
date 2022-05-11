67b27b02994860b85a8bc9ec0a8504a79fcf5245
Shannapedia <96964900+Shannapedia@users.noreply.github.com>
2022-05-11 17:37:04 -0600

    Deleted the comments on the deck file

--- hilo/game/deck.py
+++ hilo/game/deck.py
@@ -1,33 +1,3 @@
-# import random
-
-
-# class Deck:
-#     """A deck of cards, all values are between 1 and 13
-
-#     The responsibility of Deck is to keep track of the card that's been dealt
-   
-#     Attributes:
-#         value (int): The number of the card.
-#     """
-
-#     def __init__(self):
-#         """Constructs a new instance of Deck.
-
-#         Args:
-#             self (Deck): An instance of Decks.
-#         """
-#         self.value = 0
-#         self.points = 0
-
-#     def draw(self):
-#         """Generates a new random value for the card.
-        
-#         Args:
-#             self (Deck): An instance of Deck.
-#         """
-#         self.value = random.randint(1, 14)
-#         self.points = 50 if self.value == 5 else 100 if self.value == 1 else 0
-
 import random
 
 
