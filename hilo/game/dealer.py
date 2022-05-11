a9822bf128098cdbb132eba410ac289205ac7e9f
Cristian Avendaño <cristianmrcl@gmail.com>
2022-05-11 21:18:24 +0200

    updated a few things here and there

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -21,8 +21,8 @@ class Dealer:
         """
         self.dice = []
         self.is_playing = True
-        self.score = 0
-        self.total_score = 0
+        self.score = 300
+        self.total_score = 300
 
         for i in range(5):
             Deck = Deck()
@@ -48,35 +48,53 @@ class Dealer:
         roll_dice = input("Roll dice? [y/n] ")
         self.is_playing = (roll_dice == "y")
        
-    def do_updates(self):
-        """Updates the player's score.
+    # def do_updates(self):
+    #     """Updates the player's score.
 
-        Args:
-            self (Dealer): An instance of Dealer.
-        """
-        if not self.is_playing:
-            return 
+    #     Args:
+    #         self (Dealer): An instance of Dealer.
+    #     """
+    #     if not self.is_playing:
+    #         return 
 
-        for i in range(len(self.dice)):
-            Deck = self.dice[i]
-            Deck.roll()
-            self.score += Deck.points 
-        self.total_score += self.score
+    #     for i in range(len(self.dice)):
+    #         Deck = self.dice[i]
+    #         Deck.roll()
+    #         self.score += Deck.points 
+    #     self.total_score += self.score
 
-    def do_outputs(self):
-        """Displays the dice and the score. Also asks the player if they want to roll again. 
 
-        Args:
-            self (Dealer): An instance of Dealer.
-        """
-        if not self.is_playing:
-            return
+    # def do_outputs(self):
+    #     """Displays the dice and the score. Also asks the player if they want to roll again. 
+
+    #     Args:
+    #         self (Dealer): An instance of Dealer.
+    #     """
+    #     if not self.is_playing:
+    #         return
         
-        values = ""
-        for i in range(len(self.dice)):
-            Deck = self.dice[i]
-            values += f"{Deck.value} "
+    #     values = ""
+    #     for i in range(len(self.dice)):
+    #         Deck = self.dice[i]
+    #         values += f"{Deck.value} "
 
-        print(f"You rolled: {values}")
-        print(f"Your score is: {self.total_score}\n")
-        self.is_playing == (self.score > 0)
+    #     print(f"You rolled: {values}")
+    #     print(f"Your score is: {self.total_score}\n")
+    #     self.is_playing == (self.score > 0)
+
+
+    def points(self):
+        """This displays the latest card and the score. If score isn't equal to 0, it will ask if user wants to keep playing"""
+        points_total=300
+
+        if points_total > 0:
+            first_command = input(f"The current points are {points_total}. Want to draw a card? Y/N")
+            
+            if first_command == "Y":
+                #here comes high_or_low, if correct then here we'll add value to points total, if incorrect remove it. Then we loop back to the beggining with the correct point total.
+                print("just filling space with this")
+
+            else:
+                print("No worries. Game over")
+        else:
+            print(f"Score is 0 or lower-- Game over!")
